93e6e41e8d2bf4e628aa90d89c21240f595b3430
Daniel Mau <daniel.b.mau@gmail.com>
2024-12-03 09:53:18 -0500

    Solved Day 3 Part 2

--- aoc2024/day_3/solution.py
+++ aoc2024/day_3/solution.py
@@ -7,7 +7,7 @@ def parse(input_file):
     return input
 
 def part1(input):
-    pattern = r"mul\(\d+\,\d+\)" 
+    pattern = r"mul\(\d+\,\d+\)" #Learning REGEX 
     x = re.findall(pattern, input)
     print(x)
 
@@ -23,10 +23,36 @@ def part1(input):
     print(sum)
 
 def part2(input):
-   pattern = r"don't\(\)" 
-   x = re.findall(pattern, input)
-   print(x)
+    pattern = r"mul\(\d+\,\d+\)|don't\(\)|do\(\)" #Alternator key | very nice 
+    x = re.findall(pattern, input)
+    print(x)
 
+    sum = 0
+    enabled = True
+    y = []
+
+    for i in range(0, len(x)):
+        if x[i] == "don't()":
+            enabled = False
+            i += 1
+        elif x[i] == "do()":
+            enabled = True
+            i += 1
+        else:
+            if enabled == True:
+                y.append(x[i])
+            else:
+                continue
+    
+    for n in y:
+        pattern = r"\d+"
+        y = re.findall(pattern, n)
+        print(y)
+        product = int(y[0]) * int(y[1])
+        print(product)
+        sum += product 
+
+    print(sum)
 if __name__ == "__main__":
     for input_file in sys.argv[1:]:
         part1(parse(input_file))
